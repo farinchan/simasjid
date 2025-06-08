52a2ca6c84dc07322b3307fc56bcf99e5608be57
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-06-08 09:45:52 +0700

    feat: required packkage install

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Jenssegers\Agent\AgentServiceProvider::class,
+    Maatwebsite\Excel\ExcelServiceProvider::class,
+    RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+    Stevebauman\Location\LocationServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
+    Alkoumi\LaravelHijriDate\LaravelHijriDateServiceProvider::class,
 ];
